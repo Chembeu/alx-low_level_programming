7f6b55eff5252915b0c8de9421ef0a7aa2fc5909
root <simon.shanga1@gmail.com>
2023-10-08 21:37:04 -0700

    all num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/* this program will output all the numbers in base 10
+ * the numbers will be from the smallest to the largest
+ * 
+ */
+int main()
+{
+int num = 0;
+
+for(num =0; num < 10; num++)
+{
+	printf("%d",num);
+	printf("\n");
+	return 0;
+}
+}
