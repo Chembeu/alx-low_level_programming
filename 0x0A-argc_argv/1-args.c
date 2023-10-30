c70c038f7a4d8acade14aa51d87ca8c847b3c68a
root <simon.shanga1@gmail.com>
2023-10-30 08:50:07 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	printf("argc=%d\n",argc);
+	int i;
+
+	(void)argv;
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+			;
+		printf("%d\n", i - 1);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
 }
