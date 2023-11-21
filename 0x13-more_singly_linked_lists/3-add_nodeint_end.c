4279a102c2a6c7182beadb6871a4ea59f3638479
root <simon.shanga1@gmail.com>
2023-11-20 23:52:10 -0800

    3-add_nodeint_end.c

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *temp, *cursor;
+
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		return (NULL);
+
+	temp->next = NULL;
+	temp->n = n;
+
+	if (*head)
+	{
+		cursor = *head;
+		while (cursor->next != NULL)
+			cursor = cursor->next;
+		cursor->next = temp;
+	}
+	else
+		*head = temp;
+	return (temp);
+}
