a4d3d334d5ae871904f8b74af2145eb65d9d126e
root <simon.shanga1@gmail.com>
2024-01-15 11:50:06 -0800

    second

--- 0x18-dynamic_libraries/0-isupper.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-/* this fuction will print all the uppercase alphabet letters
- * and return the same when called
- */
-int _isupper(int c)
-{
-	return (c >= 'A' && c <= 'Z');
-}
